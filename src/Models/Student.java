81fa5f0b682ea0c26921a7d4f2d1d2b6dea12ebb
Unknownplaylist <44828267+Unknownplaylist@users.noreply.github.com>
2023-11-03 12:54:27 +0800

    Update Student.java

---
+++ src/Models/Student.java
@@ -0,0 +1,35 @@
+class Student {
+    private String name;
+    private String email;
+    private String faculty;
+    
+    public Student(String name, String email, String faculty) {
+        this.name = name;
+        this.email = email;
+        this.faculty = faculty;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getFaculty() {
+        return faculty;
+    }
+    
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+}
