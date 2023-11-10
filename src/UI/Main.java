46790596c42bc41953938d059ccca60dc439abb9
Poruri Shravan <149792075+shp314@users.noreply.github.com>
2023-11-10 12:08:19 +0800

    updated main, along with staff and student view

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -1,68 +1,65 @@
-import Controllers.*;
-
-public class Main {
-    public static void main(String[] args) {
-        // Initialize the StudentsController and StaffController
-        StudentsController studentsController = new StudentsController();
-        StaffController staffController = new StaffController(); // Assuming StaffController is implemented
-
-        // Initialize the LoginController with both StudentsController and StaffController
-        LoginController loginController = new LoginController(studentsController, staffController);
-
-        // Sample user ID and password (the ID is the part before "@")
-        String userId = "HUKUMAR"; // Assuming "KOH1" is the ID part of the email
-        String password = "password";
-
-        // Attempt to log in the user
-        boolean loggedIn = loginController.loginUser(userId, password);
-
-        if (loggedIn) {
-            // User is successfully logged in and is recognized as a student or staff
-            // Access student-specific or staff-specific functionalities here
-
-            // Example: Get the student's or staff's name and faculty using the complete email
-            String studentEmail = userId + "@e.ntu.edu.sg";
-            String staffEmail = userId + "@NTU.EDU.SG";
-            
-            String name;
-            String faculty;
-
-            // Determine if the user is a student or staff and get the name and faculty
-            if (studentsController.verifyStudent(studentEmail)) {
-                name = studentsController.getStudentName(studentEmail);
-                faculty = studentsController.getStudentFaculty(studentEmail);
-            } else {
-                name = staffController.getStaffName(staffEmail);
-                faculty = staffController.getStaffFaculty(staffEmail);
-            }
-
-            // Display the name and faculty
-            System.out.println("Name: " + name);
-            System.out.println("Faculty: " + faculty);
-        }
-    }
-}
-
-/**
+package UI;
+import java.util.*;
 import Controllers.*;
 import Models.*;
 
 public class Main {
-    public static void main(String[] args) {
-        // Initialize the StudentsController
-        StudentsController studentsController = new StudentsController();
+    StaffView staffview;
+    StudentView studentview;
+    static Scanner sc = new Scanner(System.in);
+    
+    public void UIlogIn(LoginController lgc,StudentsController sdc,StaffController sfc){
+        System.out.print("Username : ");
+        String userId = sc.next();
+        System.out.print("Password : ");
+        String password = sc.next();
+        System.out.println();
+        boolean loggedIn = lgc.loginUser(userId, password);
 
-        // Create a new student object
-        String name = "John";
-        String email = "john@e.ntu.edu.sg";
-        String faculty = "EEE";
+        if(loggedIn){
+            String user = lgc.userType(userId);
+            if(user.equals("Student")){
+                studentview = new StudentView(userId,sdc);
+                studentview.display();
+            }
+            else{
+                staffview = new StaffView(userId,sfc);
+                staffview.display();
+            }
+        }
         
-        Student newStudent = new Student(name, email, faculty);
+    }
+    public static void main(String[] args) {
+        int exit=0;
+        do
+        {
+            Main ob = new Main();
+            // Initialize the StudentsController and StaffController
+            StudentsController studentsController = new StudentsController();
+            StaffController staffController = new StaffController();
+            // Initialize the LoginController
+            LoginController loginController = new LoginController(studentsController, staffController);
 
-        // Write the new student to the database
-        studentsController.writeStudent(newStudent);
+            System.out.println("Welcome to CAMs\n===============\nPlease choose the option of your choice\n");
+            System.out.println("(1) Log In");
+            System.out.println("(2) Exit");
+            System.out.print("In : ");
+            int loginput = sc.nextInt();
+            System.out.println();
 
-        System.out.println("New student added: " + newStudent.getName());
+            switch(loginput){
+                case 1 :
+                    ob.UIlogIn(loginController,studentsController,staffController);
+                break;
+                case 2 : 
+                    System.out.println("Closing Application...");
+                    exit=1;
+                break;
+                default : 
+                    System.out.println("Invalid. Try Again");
+                break;
+            }
+            System.out.println();
+        }while (exit!=1);
     }
 }
-*/
