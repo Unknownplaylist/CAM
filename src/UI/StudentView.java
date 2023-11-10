61eaee0bba0732fc5d47ca1ad3659ce1dbf17a48
Poruri Shravan <149792075+shp314@users.noreply.github.com>
2023-11-10 15:35:49 +0800

    changed student view acc to test cases

--- src/UI/StudentView.java
+++ src/UI/StudentView.java
@@ -18,16 +18,21 @@ public class StudentView {
             System.out.println(name+"\n"+faculty+"\n");
             System.out.println(" Menu ");
             System.out.println("======");
-            System.out.println("(1) View all Camps");
-            System.out.println("(2) View your Camps\n");
-            System.out.println("(3) LogOff\n");
+            System.out.println("(1) View Camps");
+            System.out.println("(2) View your Camps");
+            System.out.println("(3) Register as Camp Committee");
+            System.out.println("(4) Withdraw");
+            System.out.println("(5) Submit Enquiries");
+            System.out.println("(6) Check Enquiries\n");
+            System.out.println("(7) Change your password");
+            System.out.println("(8) LogOff\n");
             System.out.print("In: ");
             int choice = sc.nextInt();
             switch(choice){
-                case 1:case 2:default:
+                case 1:case 2:case 3:case 4:case 5:case 6:case 7:default:
                     System.out.println("Needs Implementation!");
                 break;
-                case 3:
+                case 8:
                     logOff=1;
                     System.out.println("Logging Off...");
                 break;
