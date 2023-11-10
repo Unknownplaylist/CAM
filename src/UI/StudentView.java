9cf76fc4e6fc3d03c57c749e9ae1e9806d2853e4
Poruri Shravan <149792075+shp314@users.noreply.github.com>
2023-11-10 12:09:58 +0800

    updated main, along with staff and student view

--- src/UI/StudentView.java
+++ src/UI/StudentView.java
@@ -4,13 +4,36 @@ import Controllers.*;
 import Models.*;
 
 public class StudentView {
+    static Scanner sc=new Scanner(System.in);
     String userid, name, faculty;
-    public StudentView(String id){
+    public StudentView(String id,StudentsController studentcont){
         userid=id;
+        name=studentcont.getStudentName(studentcont.getStudentMail(id));
+        faculty=studentcont.getStudentFaculty(studentcont.getStudentMail(id));
 
     }
     public void display(){
-        System.out.println(userid);
+        int logOff=0;
+        do{
+            System.out.println(name+"\n"+faculty+"\n");
+            System.out.println(" Menu ");
+            System.out.println("======");
+            System.out.println("(1) View all Camps");
+            System.out.println("(2) View your Camps\n");
+            System.out.println("(3) LogOff\n");
+            System.out.print("In: ");
+            int choice = sc.nextInt();
+            switch(choice){
+                case 1:case 2:default:
+                    System.out.println("Needs Implementation!");
+                break;
+                case 3:
+                    logOff=1;
+                    System.out.println("Logging Off...");
+                break;
+            }
+            System.out.println("\n");
+        }while(logOff!=1);
     }
 
 }
