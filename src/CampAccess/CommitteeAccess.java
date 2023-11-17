7c2fa21d5af0e530a8a42812e876f14a26215eb1
Unknownplaylist <dariuslm755@gmail.com>
2023-11-17 20:19:51 +0800

    Refactor CommitteeAccess class and add new methods and variables

--- src/CampAccess/CommitteeAccess.java
+++ src/CampAccess/CommitteeAccess.java
@@ -12,67 +12,63 @@ public class CommitteeAccess {
     private SuggestionController suggestion_controller;
     private EnquiryController enquiry_controller;
     private StudentsController student_controller;
-
-    public CommitteeAccess(SuggestionController suggestion_controller, EnquiryController enquiry_controller,
-        StudentsController student_controller){
-        this.suggestion_controller = suggestion_controller;
-        this.enquiry_controller = enquiry_controller;
-        this.student_controller = student_controller;
-    }
-    
-    public void viewCamps(){
-
-    }
-
-    public void viewYourCamps(){
-
-    }
-        
-    public void viewCampDetails(){
-
-    }
-    
-    //SuggestionController
-    public void submitSuggestion(String campCommName){ 
-        suggestion_controller.createSuggestion(campCommName);
-    }
-    public void viewSuggestion(String campCommName){
-        suggestion_controller.viewSuggestion(campCommName);
-    }
-    public void editSuggestion(String campCommName){
-        suggestion_controller.editSuggestion(campCommName);
-
-    }
-    public void deleteSuggestion(String studentEmail){//to extend the suggestion controller to take in camp
-        String studentName = student_controller.getStudentName(studentEmail);
-        suggestion_controller.editSuggestion(studentName);
-    }
-
-    //EnquiryController
-    public void checkEnquiry(String camp){
-        //get his camp
-        List<String[]> unrepliedEnquiriesList = enquiry_controller.execFindUnrepliedEnquiry(camp);
-        enquiry_controller.formatMessageList(unrepliedEnquiriesList);
-    }
-    public void viewEnquiry(String camp){
-        enquiry_controller.viewReplyToEnquiry(camp);
-    }
-    public void replyEnquiry(String camp){
-        enquiry_controller.execReplyEnquiry(camp);
-    }
-
-
-
     private CampController campController;
     private StudentsController studentController;
 
     public CommitteeAccess(SuggestionController suggestionController, EnquiryController enquiryController,
-                           StudentsController studentController, CampController campController){
+            StudentsController studentController, CampController campController) {
         this.suggestion_controller = suggestionController;
         this.enquiry_controller = enquiryController;
         this.student_controller = studentController;
         this.campController = campController;
     }
+
+    public void viewCamps() {
+
+    }
+
+    public void viewYourCamps() {
+
+    }
+
+    public void viewCampDetails() {
+
+    }
+
+    // SuggestionController
+    public void submitSuggestion(String campCommName) {
+        suggestion_controller.createSuggestion(campCommName);
+    }
+
+    public void viewSuggestion(String campCommName) {
+        suggestion_controller.viewSuggestion(campCommName);
+    }
+
+    public void editSuggestion(String campCommName) {
+        suggestion_controller.editSuggestion(campCommName);
+
+    }
+
+    public void deleteSuggestion(String studentEmail) {// to extend the suggestion controller to take in camp
+        String studentName = student_controller.getStudentName(studentEmail);
+        suggestion_controller.editSuggestion(studentName);
+    }
+
+    // EnquiryController
+    public void checkEnquiry(String camp) {
+        // get his camp
+        List<String[]> unrepliedEnquiriesList = enquiry_controller.execFindUnrepliedEnquiry(camp);
+        enquiry_controller.formatMessageList(unrepliedEnquiriesList);
+    }
+
+    public void viewEnquiry(String camp) {
+        enquiry_controller.viewReplyToEnquiry(camp);
+    }
+
+    public void replyEnquiry(String camp) {
+        enquiry_controller.execReplyEnquiry(camp);
+    }
+
     public void generateStudentList(String studentEmail) {
         // Assuming 'getStudentByEmail' method exists in StudentsController
         Student committeeMember = studentController.getStudentByEmail(studentEmail);
@@ -81,7 +77,6 @@ public class CommitteeAccess {
             return;
         }
 
-        
         Camp camp = campController.getCampByCommitteeMember(committeeMember.getName());
         if (camp == null) {
             System.out.println(committeeMember.getName() + " is not a committee member of any camp.");
